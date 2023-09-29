d38a4aea1af47515584cb3b729f3ad5f0896d4de
rubroshe <rubroshe@iu.edu>
2023-09-28 22:37:45 -0400

    Hour Checkpoint in Video

--- 3D Tut/Assets/Scripts/GameSetup.cs
+++ 3D Tut/Assets/Scripts/GameSetup.cs
@@ -45,5 +45,61 @@ public class GameSetup : MonoBehaviour
         Vector3 firstInRowPosition = headBallPosition.position;
         Vector3 currentPosition = firstInRowPosition;
 
+        void PlaceRedBall(Vector3 position)
+        {
+            GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+            ball.GetComponent<Ball>().BallSetup(true);
+            redBallsRemaining--;
+        }
+
+        void PlaceBlueBall(Vector3 position)
+        {
+            GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+            ball.GetComponent<Ball>().BallSetup(false);
+            blueBallsRemaining--;
+        }
+        // Outer loop is the 5 rows
+        for (int i = 0; i < 5; i++)
+        {
+            // Inner loop are the balls in each row
+            for (int j = 0; j < NumInThisRow; j++)
+            {
+                // Check to see if it's the middle spot where 8ball goes
+                if (i == 2 && j == 1)
+                {
+                    PlaceEightBall(currentPosition);
+                }
+                // If there are red and blue balls remaining, randomly choose one and place it
+                else if (redBallsRemaining > 0 && blueBallsRemaining > 0)
+                {
+                    rand = Random.Range(0, 2);
+                    if (rand == 0)
+                    {
+                        PlaceRedBall(currentPosition);
+                    }
+                    else
+                    {
+                        PlaceBlueBall(currentPosition);
+                    }
+                }
+                // If only red balls are left, place one
+                else if (redBallsRemaining > 0)
+                {
+                    PlaceRedBall(currentPosition);
+                }
+                // Otherwise, place a blue ball
+                else
+                {
+                    PlaceBlueBall(currentPosition);
+                }
+                // Move the current position for the next ball in this row to the right
+                currentPosition += new Vector3(1, 0, 0).normalized * ballDiameter;
+            }
+
+            // Once all the balls in the row have been placed, move to the next row
+            firstInRowPosition += Vector3.back * (Mathf.Sqrt(3) * ballRadius) + Vector3.left * ballRadius;
+            currentPosition = firstInRowPosition;
+            NumInThisRow++;
+        }
     }
 }
